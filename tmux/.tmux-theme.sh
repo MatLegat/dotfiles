0e114c228f3567069ef14b617a9b1eef21a7071f
Matteus Legat <matteuslegat@gmail.com>
2016-08-03 23:32:14 -0300

    Import TMUX

---
+++ tmux/.tmux-theme.sh
@@ -0,0 +1,26 @@
+# Start windows and panes at 1, not 0
+set -g base-index 1
+set -g pane-base-index 1
+
+set-option -g repeat-time 0
+
+# Removes ESC delay
+set -sg escape-time 0
+
+# List of plugins
+set -g @tpm_plugins '                 \
+     matlegat/maglev                  \
+     tmux-plugins/tpm                 \
+     tmux-plugins/tmux-sensible       \
+     tmux-plugins/tmux-resurrect      \
+     tmux-plugins/tmux-continuum      \
+     tmux-plugins/tmux-yank           \
+     tmux-plugins/tmux-pain-control   \
+     tmux-plugins/tmux-copycat        \
+     tmux-plugins/tmux-open           \
+     tmux-plugins/tmux-battery        \
+     tmux-plugins/tmux-cpu            \
+'
+
+# Initialize TMUX plugin manager
+run '~/.tmux/plugins/tpm/tpm'
---
+++ tmux/install.sh
@@ -0,0 +1,6 @@
+# install TMUX plugin manager
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm > /dev/null
+
+# install TMUX plugins
+~/.tmux/plugins/tpm/scripts/install_plugins.sh
+
