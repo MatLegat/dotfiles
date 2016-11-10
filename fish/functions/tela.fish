8884b2d06af28868175ac535cc8d7144f8c800a5
Matteus Legat <matteuslegat@gmail.com>
2016-11-10 13:54:48 -0200

    Update screen position

--- fish/functions/tela.fish
+++ fish/functions/tela.fish
@@ -2,6 +2,6 @@ function tela
     if [ (xrandr | grep -E "VGA-1 connected 1") ]
         xrandr --output VGA-1 --off
     else
-        xrandr --output VGA-1 --auto --pos 1920x200
+        xrandr --output VGA-1 --auto --pos 1920x00
     end
 end
